4d26cf795f84e3e33067fb7da1c76ac70f91d913
vinthen <nehtniv@gmail.com>
2019-04-06 09:57:15 +0800

    add is mobile

--- js/index.js
+++ js/index.js
@@ -3,30 +3,41 @@ const wrapper = document.querySelector('#main');
 const header = document.querySelector('.header');
 
 
+// console.log(navigator.userAgent);
 
 
 
+showViewportInfo();
 
-showWindowSize();
+showUserAgent();
+
+
 
 window.addEventListener('resize',() => {
 
-    showWindowSize();
+    showViewportInfo();
 
 });
 
 
-
-
-function showWindowSize(){
+function showViewportInfo(){
 
     const ww = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
     const wh = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-
-    // console.log(ww,wh);
+  
     header.innerHTML = `<span class="width">${ww}</span> x ${wh}`;
+}
 
+function showUserAgent(){
 
+    const userAgentData = navigator.userAgent;
+    
+    document.querySelector('.userInfo').textContent = 
+    `${userAgentData}`;
 
+    const isMobile = /mobile/i.test(userAgentData);
+    if(isMobile){
+        document.querySelector('.isMobile').textContent = '(Mobile)';
+    }
 }
